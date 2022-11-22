e74fa6e9959fdcaf07203601b3c78801effa0ec7
fabianhansch <fabian.hansch@gmail.com>
2022-11-22 14:20:45 +0100

    Tasks now added on Submit

--- src/Components/ListItem/ListItem.js
+++ src/Components/ListItem/ListItem.js
@@ -2,23 +2,35 @@ import "./ListItem.css";
 import { useState } from "react";
 
 export function ListItem() {
-    const [items, setItems] = useState(["Test"]);
+    const [items, setItems] = useState([]);
+    const [value, setValue] = useState("");
 
-    const handleSubmit = ({target}) => {
-        setItems((prev) => {
-            [...prev, target]
-        });
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addItem(value);
+        setValue("");
     };
+
+    const addItem = (textInput) => {
+        const newItemList = [...items, textInput];
+        setItems(newItemList);
+    }
+
+    const changeValue = (e) => {
+        setValue(e.target.value);
+    }
     
     return(
         <div>
             <ul>
-                {items}
+                {items.map((item, index) => {
+                    return <li key={index}>{item}</li>
+                })}
             </ul>
 
             <form onSubmit={handleSubmit}>
                 <label htmlFor="newTask">New Task</label>
-                <input id="newTask" type="text"/>
+                <input id="newTask" type="text" value={value} onChange={changeValue}/>
                 <input type="submit" value="Submit" />
             </form>
         </div>
